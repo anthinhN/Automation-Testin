7b7dbc0e7d50ad39dfe1b73d69059fff8633d4f7
anthinhN <144272204+anthinhN@users.noreply.github.com>
2025-07-23 16:26:12 -0400

    Delete subnet 2 directory

--- subnet 2/GetSubnetListData.java
+++
@@ -1,9 +0,0 @@
-package dto.vServerr.han01.response.subnet;
-import dto.vServerr.request.subnet.DataSubnet;
-import lombok.Getter;
-import java.util.ArrayList;
-
-@Getter
-public class GetSubnetListData {
-    public ArrayList<DataSubnet> listData;
-}
